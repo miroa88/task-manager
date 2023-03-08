227f619c3065d331a781cde70f7bcd1b6cd61937
Miro Abdalian <miroabdalian@gmail.com>
2023-03-07 19:29:30 -0800

    completeing crud operation

--- backend/controllers/taskController.js
+++ backend/controllers/taskController.js
@@ -1,11 +1,11 @@
-const Task = require("../model/taskModel");
+const Task = require("../models/taskModel");
 
 const createTask = async (req, res) => {
     try {
         const task = await Task.create(req.body);
         res.status(200).json(task);
     } catch (err) {
-        res.status(500).json({msg: err.message})
+        res.status(500).json({msg: err.message});
     }
 }
 
@@ -14,8 +14,50 @@ const getAllTasks =  async (req, res) => {
         const tasks = await Task.find();
         res.status(200).json(tasks);
     } catch (err) {
-        res.status(500).json({msg: err.message})
+        res.status(500).json({msg: err.message});
     }
 }
 
-module.exports = {createTask, getAllTasks};
+const getTaskById = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const task = await Task.findById(id);
+        if(!task) {
+            return res.status(404).json(`No task associated with id ${id}`);
+        }
+        res.status(200).json(task);
+    } catch (err) {
+        res.status(500).json({msg: err.message});
+    }
+}
+
+
+const deleteTaskById = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const task = await Task.findByIdAndDelete(id);
+        if(!task) {
+            return res.status(404).json(`No task associated with id ${id}`);
+        }
+        res.status(200).json("Task deleted");
+    } catch (err) {
+        res.status(500).json({msg: err.message});
+    }
+}
+
+const editTaskById = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const task = await Task.findByIdAndUpdate(
+            {_id: id}, req.body, {new: true, runValidators: true}
+        );
+        if(!task) {
+            return res.status(404).json(`No task associated with id ${id}`);
+        }
+        res.status(200).json(task);
+    } catch (err) {
+        res.status(500).json({msg: err.message});
+    }
+}
+
+module.exports = {createTask, getAllTasks, getTaskById, deleteTaskById, editTaskById};
--- backend/routes/taskRoute.js
+++ backend/routes/taskRoute.js
@@ -1,5 +1,11 @@
 const express = require("express");
-const { createTask, getAllTasks } = require("../controllers/taskController")
+const { 
+    createTask, 
+    getAllTasks, 
+    getTaskById, 
+    deleteTaskById,
+    editTaskById
+} = require("../controllers/taskController");
 const router = express.Router();
 
 //create a task
@@ -7,5 +13,17 @@ router.post("/api/tasks", createTask);
 
 //get all tasks
 router.get("/api/tasks", getAllTasks);
+
+//get a task by id
+router.get("/api/tasks/:id", getTaskById);
+
+//delete a task by id
+router.delete("/api/tasks/:id", deleteTaskById)
+
+//update a task by id
+router.put("/api/tasks/:id", editTaskById)
+
+//partial update a task by id
+router.patch("/api/tasks/:id", editTaskById)
 
 module.exports = router;
